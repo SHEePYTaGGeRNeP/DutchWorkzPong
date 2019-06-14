a0317fd370ab9ffbb2f1ada8d5cad27e3ecd1dfc
Lorenzo <lorenzo.suiker@dutchworkz.nl>
2019-06-14 16:05:23 +0200

    Circle, Prefab, Added controller

---
+++ PONG Praag/Assets/Scripts/PanelController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+public class PanelController : MonoBehaviour
+{
+    //[SerializeField]
+    //private Vector2 _bounds;
+    [SerializeField]
+    private float _speed = 3f;
+
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        this._rb = this.GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float y = Input.GetAxis("Vertical");
+        float add = y * Time.deltaTime * this._speed;
+        this._rb.MovePosition(new Vector2(this.transform.position.x, this.transform.position.y + add));
+    }
+
+}
